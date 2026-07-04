396cf278d7382a0d63228fb9d5bf3449ceb32014
Nia-one <kumarajay.mahawar.1991@gmail.com>
2026-07-04 07:41:20 +0530

    Update product browser page size

--- services/productBrowser.js
+++ services/productBrowser.js
@@ -5,7 +5,7 @@ async function getProductsPage(page = 1) {
 
     const products = await getProducts();
 
-    const pageSize = 15;
+    const pageSize = 10;
 
     const start = (page - 1) * pageSize;
 
@@ -42,7 +42,7 @@ async function getProductByPageSelection(page, selection) {
 
     const products = await getProducts();
 
-   const pageSize = 15;
+   const pageSize = 10;
 
     const index = ((page - 1) * pageSize) + (selection - 1);
 
