43cf7d4ec1eae3f5f3fde548611982df0a5911b4
Nia-one <kumarajay.mahawar.1991@gmail.com>
2026-07-12 21:45:10 +0530

    Increase API rate limit for command center

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.use(cors());
 // Rate Limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
+    max: 1000,
     message: {
         success: false,
         message: "Too many requests. Please try again later."
